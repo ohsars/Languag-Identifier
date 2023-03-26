3debfad9e35edc57863471370aa319a6aa9ab8fc
ohsars <efebeosas@gmail.com>
2023-03-26 15:51:26 +0100

    Made few changes to files

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ import string
 import streamlit as st
 import webbrowser
 
+
+
 global Lrdetect_Model
 
-LrdetectFile = open('model.pckl','rb')
+LrdetectFile = open('model.pkl','rb')
 Lrdetect_Model = pickle.load(LrdetectFile)
 LrdetectFile.close()
 st.title("Yoruba Language Detection Tool")
-input_test = st.text_input("Provide your text input here", 'Hello my name is Jay ')
+input_test = st.text_input("Provide your text input here", 'fẹ́ pààrọ̀ gílóòbù iná tó wà')
 
 button_clicked = st.button("Get Language Name")
 if button_clicked:
     st.text(Lrdetect_Model.predict([input_test]))
     
+voice_button_clicked = st.button("Start Voice")
+  
